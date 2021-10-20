6c4c3620f18c2bf8e5d866412e5bb8517afaa3a2
B. Blechschmidt <github@blechschmidt.pw>
2021-10-21 00:07:21 +0200

    Use getopt for parsing the command line of the preloader

--- src/preloader.c
+++ src/preloader.c
@@ -3,21 +3,33 @@
 #include <limits.h>
 #include <unistd.h>
 #include <string.h>
+#include <getopt.h>
 
 #define LIB_NAME "freebind.so"
-#define LIB_PATH "/usr/local/lib"
+
+#ifndef LIB_DIR
+#define LIB_DIR "/usr/local/lib"
+#endif
 
 static int help(char *name)
 {
-	fprintf(stderr, "Usage: %s program [arguments]\n", name);
+	fprintf(stderr, "Usage: %s [options] program [arguments]\n"
+			"  -r  --random  IPv4 or IPv6 network in CIDR notation.\n", name);
 	return EXIT_FAILURE;
 }
 
 int main(int argc, char **argv)
 {
 	char buf[8192];
-	int first_arg = 1;
 	char *orig_env = getenv("LD_PRELOAD");
+	int opt = 0;
+	int option_index = 0;
+
+	static struct option long_options[] =
+	{
+		{"random", required_argument, 0, 'r'},
+		{0, 0, 0, 0}
+	};
 
 	if(argc < 2)
 	{
@@ -25,28 +37,36 @@ int main(int argc, char **argv)
 	}
 
 	buf[0] = 0;
-	for(int i = 1; i < argc; i++)
+
+	while((opt = getopt_long(argc, argv, "r:", long_options, &option_index)) != -1)
 	{
-		if(argv[i][0] != '-')
+		switch(opt)
 		{
-			first_arg = i;
-			break;
-		}
-		else if((strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "--random") == 0) && argv[++i] != NULL)
-		{
-			size_t buflen = strlen(buf);
-			snprintf(buf + buflen, sizeof(buf) - buflen, "%s ", argv[i]);
+			 case 'r':
+				 size_t buflen = strlen(buf);
+				 snprintf(buf + buflen, sizeof(buf) - buflen, "%s ", optarg);
+				 break;
 		}
 	}
+
+	if(optind >= argc)
+	{
+		fprintf(stderr, "No program specified.\n");
+		return EXIT_FAILURE;
+	}
+
+	buf[sizeof(buf) - 1] = 0;
 	setenv("FREEBIND_RANDOM", buf, 1);
 
-	snprintf(buf, sizeof(buf), LIB_PATH "/" LIB_NAME "%s%s",
+	snprintf(buf, sizeof(buf), LIB_DIR "/" LIB_NAME "%s%s",
 			orig_env ? " " : "",
 			orig_env ? orig_env : "");
+	buf[sizeof(buf) - 1] = 0;
 	setenv("LD_PRELOAD", buf, 1);
 
-	execvp(argv[first_arg], &argv[first_arg]);
+	execvp(argv[optind], &argv[optind]);
 	snprintf(buf, sizeof(buf), "Failed to open program \"%s\"", argv[1]);
+	buf[sizeof(buf) - 1] = 0;
 	perror(buf);
 	return help(argv[0]);
 }
